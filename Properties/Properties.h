bde16fb5c0f5f4ff352e4a15771d9cf24886d5ac
James W. Fawcett <jfawcett@twcny.rr.com>
2019-07-02 22:53:37 -0400

    major changes to the definition of Properties class

--- Properties/Properties.h
+++ Properties/Properties.h
@@ -1,14 +1,14 @@
 #pragma once
 /////////////////////////////////////////////////////////////////////
 // Properties.h - provides getter and setter methods               //
-// ver 1.1                                                         //
+// ver 1.3                                                         //
 // Jim Fawcett, CSE687 - Object Oriented Design, Fall 2018         //
 /////////////////////////////////////////////////////////////////////
 /*
 *  Package Operations:
 *  -------------------
-*  This package provides a single Property<T> class that provides
-*  getter and setter methods and backing store T t_ using the property 
+*  This package provides a single Property<T, E, C> class that provides
+*  getter and setter methods and backing store T innerVar, using the property 
 *  name.
 *  - Here's an example:
 *      Property<int> intProp;
@@ -16,12 +16,14 @@
 *      intProp()      returns the internal store value
 *  - So one declaration creates two methods with simple syntax, using
 *    the property's instance name. 
-*  - The getter and setter methods are virtual, so you can override
-*    them to provide logic that constraints getting and setting.
+*  - The getter and setter methods call constraint methods C::in and C::out
+*    to provide logic that constrains getting and setting values.
+*  - The property derives from an extension class E that can be configured
+*    to supply methods you expect to be able to call on a T type.
 *  - Property also provides the method ref() which returns a reference
 *    to the underlying stored value.  That lets us call member functions
 *    directly on the stored type.  However, that lets users bypass
-*    setter logic, so use it with caution.
+*    getter and setter logic, so use it with caution.
 *
 *  Required Files:
 *  ---------------
@@ -29,6 +31,11 @@
 *
 *  Maintenance History:
 *  --------------------
+*  ver 1.3 : 02 Jul 2019
+*  - demonstrated that template specializations of Ext and Constraint
+*    work as expected
+*  ver 1.2 : 27 Jun 2019
+*  - added Ext and Constraint classes
 *  ver 1.1 : 30 Jan 2019
 *  - added ref()
 *  ver 1.0 : 11 Oct 2018
@@ -36,6 +43,8 @@
 */
 
 #include <string>
+#include <vector>
+#include <stdexcept>
 
 namespace Utilities
 {
@@ -44,65 +53,137 @@ namespace Utilities
   // - You can define any methods that are syntactically correct
   //   for the type T
   // - Example: push_back when T is an STL container with method push_back 
+  //
+  // Ext is an externsion of Property<T> interface to include some or all
+  // of T's methods.  That works because Property<T> derives from E<T>.
 
   template <typename T>
   struct Ext {
-    Ext() : t_(T()) {}
-    Ext(const T& t) : t_(t) {}
-    std::string typeIs()
+    Ext() : innerVar(T()) {}
+    Ext(const T& t) : innerVar(t) {}
+    virtual ~Ext() {}
+    std::string typeIs() const
     {
-      return std::string("encapsulated type is ") + typeid(t_).name();
+      return typeid(innerVar).name();
     }
-    T t_;
+    size_t sizeIs() const
+    {
+      return sizeof(innerVar);
+    }
+    T innerVar;
   };
+
+  /////////////////////////////////////////////////////////////////////
+  // Template specialization of Ext<T> for std::string
+  // - supports indexing and concatenation
+
+  template <>
+  struct Ext<std::string> {
+    Ext() : innerVar(std::string()) {}
+    Ext(const std::string& t) : innerVar(t) {}
+    virtual ~Ext() {}
+    void operator+=(const std::string& t)  // no chaining  because shouldn't return reference
+    {
+      innerVar += t;
+    }
+    char& operator[](size_t n)
+    {
+      if (n < 0 || n >= innerVar.size())
+        throw std::out_of_range("index error in string property");
+      return innerVar[n];
+    }
+    char operator[](size_t n) const
+    {
+      if (n < 0 || n >= innerVar.size())
+        throw std::out_of_range("index error in string property");
+      return innerVar[n];
+    }
+    std::string innerVar;
+  };
+
+  /////////////////////////////////////////////////////////////////////
+  // Template specialization of Ext<T> for std::vector<T>
+  // - supports indexing, iteration, insertion, and push_back
 
   template <typename T>
-  struct StrExt {
-    StrExt() : t_(T()) {
-      //std::cout << "\n  constructing new StrExt instance";
+  struct Ext<std::vector<T>> {
+    Ext() : innerVar(std::vector<T>()) {}
+    Ext(const std::vector<T>& t) : innerVar(t) {}
+    virtual ~Ext() {}
+
+    using iterator = typename std::vector<T>::iterator;
+    
+    iterator begin() {
+      return innerVar.begin();
     }
-    StrExt(const T& t) : t_(t) {
-      //std::cout << "\n  constructing new StrExt instance";
+    iterator end() {
+      return innerVar.end();
     }
-    void operator+=(const T& t)  // no chaining  because shouldn't return reference
+    T pop_back()
     {
-      t_ += t;
+      return innerVar.pop_back();
     }
-    T t_;
+    void push_back(const T& t)
+    {
+      innerVar.push_back(t);
+    }
+    iterator insert(iterator iter, const T& t)
+    {
+      return innerVar.insert(iter, t);
+    }
+    T& operator[](size_t n)
+    {
+      if (n < 0 || n >= innerVar.size())
+        throw std::out_of_range("index error in vector property");
+      return innerVar[n];
+    }
+    T operator[](size_t n) const
+    {
+      if (n < 0 || n >= innerVar.size())
+        throw std::out_of_range("index error in vector property");
+      return innerVar[n];
+    }
+    std::vector<T> innerVar;
   };
 
-  ///////////////////////////////////////////////////////////////////
-  // Constraint provides inner methods for the property's
-  // getter and setter methods.
-  // - That allows you to add custom logic for the built in
-  //   property operator()(const T t) and operator()()
-  //   without changing the interface that clients use
+  /////////////////////////////////////////////////////////////////////
+  //// Constraint provides inner methods for the property's
+  //// getter and setter methods.
+  //// - That allows you to add custom logic for the built in
+  ////   property operator()(const T t) and operator()()
+  ////   without changing the interface that clients use
 
   template <typename T>
   struct Constraint {
-    Constraint(T& t) : t_(t) {}
+    Constraint(T& t) : innerVar(t) {}
     ~Constraint() {}
     virtual void in(const T& t) {
       /*-- add input constraints here --*/
-      t_ = t;
+      //std::cout << "\n  checking input constraints";
+      innerVar = t;
     }
     virtual T& out() {
       /*-- add output constraints here --*/
-      return t_;
+      //std::cout << "\n  checking output constraints";
+      return innerVar;
     }
-    T& t_;
-  };
+    T& innerVar;  // reference to E<T>'s innerVar, passed via constraint
+  };              // constructor in Property constructor initialization
 
   ///////////////////////////////////////////////////////////////////
   // Property class creates getter and setter methods
   // for an encapsulated variable with a single statement
 
-  template <typename T, typename E = Ext<T>, typename C = Constraint<T>>
-  class Property : public E
+  template <
+    typename T, 
+    template <class T> class E = Ext, 
+    template <class T> class C = Constraint
+  >
+  class Property : public E<T>
   {
   public:
-    Property() : E(), c_(E::t_) {}
-    Property(const T& t) : E(t), c_(t) {}
+    Property() : E<T>(), c_(E<T>::innerVar) {}
+    Property(T& t) : E<T>(t), c_(t) {}
     virtual ~Property() {}
 
     //----< setter assigns the state of t to the internal store >----
@@ -123,15 +204,31 @@ namespace Utilities
     }
     //----< getter that returns reference >--------------------------
     /*
-    *  - Allows us to call any member function on stored type
-    *  - Note that this allows a user to bypass all logic in setter.
-    *  - The Ext class is provided to allow us to avoid use of ref()
+    *  - Allows us to call any member function on stored type - that's good.
+    *  - Note that this also allows user to bypass constraint logic - that's bad.
+    *    So we shouldn't use it normally. ref() is backdoor access to encapsulated
+    *    variable and may be removed later.
+    *  - The Ext class is provided to allow us to avoid use of ref(), but causes
+    *    user more effort that we'd like. See specialization for std::string, 
+    *    and std::vector<T>, above.
     */
     virtual T& ref()
     {
-      return E::t_;
+      return E<T>::innerVar;
     }
   private:
-    C c_;
+    C<T> c_;
   };
+
+  template <typename T>
+  std::string showType(const Property<T>& t)
+  {
+    return std::string("\n  encapsulated type is ") + t.typeIs();
+  }
+
+  template <typename T>
+  std::string showSize(const Property<T>& t)
+  {
+    return std::string("\n  size of encapsulated type is ") + std::to_string(t.sizeIs());
+  }
 }
